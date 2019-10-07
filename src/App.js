76ef83cbebad7ccdfc84548f3922e4ee181c6f5d
AbhishekKalavadiya <abhishek.kalavadiya.17@gmail.com>
2019-10-07 11:15:13 +0530

    Second Commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import BusinessList from './components/BusinessList/BusinessList.js'
+import SearchBar from './components/SearchBar/SearchBar.js'
+
+const business = {
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+}
+
+let businesses = [
+	business,
+	business,
+	business,
+	business,
+	business,
+	business
+]
+
+class App extends React.Component {
+
+	searchYelp(term, location, sortBy){
+        console.log('searching Yelp with ' + term + ', ' + location + ', ' + sortBy + '.' )
+    }
+
+	render(){
+		return (
+			<div className="App">
+				<h1>Ravenous</h1>
+				<SearchBar searchYelp={this.searchYelp} />
+				<BusinessList businesses = {businesses} />
+			</div>
+	);
+}
+}
+export default App;
